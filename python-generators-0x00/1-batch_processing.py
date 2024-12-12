e2b01cdec518acbe03501f9ff0e2b2da7eb47529
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-12-12 23:49:39 +0300

    Batch processing Large Data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+def connect_db():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="ALX_prodev"
+    )
+    
+    
+# Functio to stream users in batches from the database
+def stream_users_in_batches(batch_size):
+    connection=connect_db()
+    cursor = connection.cursor(dictionary=True)
+    
+    cursor.execute("SELECT * FROM user_data")
+    
+    while True:
+        # Fetch the next batch of rows
+        rows = cursor.fetchmany(batch_size)
+        
+        if not rows:
+            break
+        
+        yield rows
+    cursor.close()
+    connection.close()
+    
+    
+# Function to process each batch and filter users over the age of 25
+def batch_processinng(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        filtered_batch = [user for user in batch if user['age'] > 25]
+        
+        yield filtered_batch
+        
+
+if __name__ == "__main__":
+    batch_size = 5
+    
+    for processed_batch in batch_processinng(batch_size):
+        print(processed_batch)
